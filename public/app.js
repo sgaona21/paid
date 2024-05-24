2678f171a432e8646189d32a7dcfb6fbc644111b
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2024-05-23 21:11:44 -0700

    headers ADDED

--- public/app.js
+++ public/app.js
@@ -1,42 +1,4 @@
 console.log("JS Linked")
 
-/////GLOBAL VARIABLES/////
-const subs = document.getElementById("subs");
-const monthlyExdpensesNav = document.getElementById("monthly-expenses-nav");
-const totalDebt = document.getElementById("total-debt");
-const expenseList = document.querySelector(".expense-list");
-const addExpense = document.querySelector(".add-expense");
+/********************GLOBAL VARIABLES********************/
 
-
-
-
-
-subs.addEventListener("click", () => {
-    if (subs.style.color = "white") {
-    subs.style.color = "aquamarine";
-    subs.style.borderBottom = "solid 3px aquamarine";
-    subs.style.position = "relative";
-    subs.style.top = "3px";
-    } else {
-        subs.style.color = "white";
-        subs.style.borderBottom = "solid 3px white";
-        subs.style.position = "relative";
-        subs.style.top = "3px";
-    }
-})
-
-subs.addEventListener("click", () => {
-    if (subs.style.color === "aquamarine") {
-        subs.style.color = "white";
-    }
-})
-
-addExpense.addEventListener("click", () => {
-    let newLi = document.createElement('li');
-    newLi.className = "add-expense";
-    let newExpense = document.createElement("input");
-    newExpense.type = "text";
-    newLi.appendChild(newExpense);
-    expenseList.insertBefore(newLi, addExpense);
-    newExpense.focus();
-});

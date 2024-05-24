eef3c51ef0fbe0ac54291e076545f3e3eb06eb54
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2024-05-24 16:24:20 -0700

    added javascript

--- public/app.js
+++ public/app.js
@@ -1,3 +1,34 @@
 console.log("JS Linked")
 
 /********************GLOBAL VARIABLES********************/
+
+const plusButton = document.getElementById("initial-add");
+const expenseSection = document.querySelector(".section-two");
+
+
+
+
+plusButton.addEventListener("click", () => {
+    let newExpenseBar = `<div class="user-input-expenses">
+    <div>
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+    </div>
+</div>`
+    plusButton.insertAdjacentHTML('beforebegin', newExpenseBar)
+
+})

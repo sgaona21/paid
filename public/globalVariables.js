af949b1c4755e4b27608a0f210c5bb049e08e37f
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-19 16:52:04 -0700

    delete button

--- public/globalVariables.js
+++
@@ -1,6 +0,0 @@
-const expenseInput = document.querySelector(".expense-input");
-const expenseAmount = document.querySelector(".expense-amount-input");
-const checked = document.querySelector(".checked");
-const expensesContainer = document.querySelector(".user-input-expenses")
-const addButton = document.getElementById("add-button");
-const expenseInputContainer = document.querySelector(".expense-input-container")

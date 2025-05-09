d0840994cbfa582700b4f2bc21822d0287cd26b0
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-05-08 20:48:52 -0700

    adjusted database data to sort before displaying markup

--- js/app.js
+++ js/app.js
@@ -74,88 +74,73 @@ addButton.addEventListener('click', () => {
 
 fetch('./data.txt')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => console.log(data.length))
 
 fetch('./data.txt')
   .then(response => response.json())
   .then(data => {
-    data.forEach(item => loadFromPoorDatabase(item));
+    sortByIndex(data)
+    loadFromPoorDatabase(data)
+    
   });
 
 
 
 function loadFromPoorDatabase(data) {
-    console.log(expenseInputcontainer.children.length)
-    let currentNumberOfChildren = expenseInputcontainer.children.length;
 
-    let userInputExpenses = document.createElement('div');
-    userInputExpenses.classList.add('user-input-expenses');
-    userInputExpenses.dataset.index = Number(currentNumberOfChildren + 1)
-    console.log('new data set index: ' + userInputExpenses.dataset.index)
+    for (let i = 0; i < data.length; i++) {
+        let userInputExpenses = document.createElement('div');
+        userInputExpenses.classList.add('user-input-expenses');
+        userInputExpenses.dataset.index = data[i].index
+        console.log('new data set index: ' + userInputExpenses.dataset.index)
 
-    let expenseContainer = document.createElement('div');
+        let expenseContainer = document.createElement('div');
+        
+        let expenseInput = document.createElement('input');
+        expenseInput.type = 'text';
+        expenseInput.classList.add('expense-input');
+        expenseInput.value = data[i].expense;
+
+        let amountcontainer = document.createElement('div');
+        amountcontainer.classList.add('amount');
+
+        let amountInput = document.createElement('input');
+        amountInput.type = 'text';
+        amountInput.classList.add('expense-amount-input');
+        amountInput.value = data[i].amount;
+
+        let checkBoxContainer = document.createElement('div');
+        checkBoxContainer.classList.add('check')
+
+        let checkBoxInput = document.createElement('input');
+        checkBoxInput.type = 'checkbox';
+        checkBoxInput.classList.add('checked');
+        checkBoxInput.checked = data[i].isPaid;
+
+        let deleteButton = document.createElement('div');
+        deleteButton.classList.add('delete');
+        deleteButton.textContent = '❌';
+
+
+        expenseContainer.appendChild(expenseInput);
+        amountcontainer.appendChild(amountInput);
+        checkBoxContainer.appendChild(checkBoxInput);
+        userInputExpenses.append(expenseContainer, amountcontainer, checkBoxContainer, deleteButton);
+        expenseInputcontainer.appendChild(userInputExpenses);
+
+        deleteButton.addEventListener('click', (e) => {
+            e.target.parentNode.remove();
+        })
+
+        userInputExpenses.addEventListener('mouseover', () => {
+            deleteButton.style.visibility = 'visible';
+        })
+
+        userInputExpenses.addEventListener('mouseleave', () => {
+            deleteButton.style.visibility = 'hidden';
+        })
+    }
     
-    let expenseInput = document.createElement('input');
-    expenseInput.type = 'text';
-    expenseInput.classList.add('expense-input');
-    expenseInput.value = data[0].name;
-
-    let amountcontainer = document.createElement('div');
-    amountcontainer.classList.add('amount');
-
-    let amountInput = document.createElement('input');
-    amountInput.type = 'text';
-    amountInput.classList.add('expense-amount-input');
-    amountInput.value = data[0].amount;
-
-    let checkBoxContainer = document.createElement('div');
-    checkBoxContainer.classList.add('check')
-
-    let checkBoxInput = document.createElement('input');
-    checkBoxInput.type = 'checkbox';
-    checkBoxInput.classList.add('checked');
-    checkBoxInput.checked = data[0].paid;
-
-    let deleteButton = document.createElement('div');
-    deleteButton.classList.add('delete');
-    deleteButton.textContent = '❌';
-
-
-    expenseContainer.appendChild(expenseInput);
-    amountcontainer.appendChild(amountInput);
-    checkBoxContainer.appendChild(checkBoxInput);
-    userInputExpenses.append(expenseContainer, amountcontainer, checkBoxContainer, deleteButton);
-    expenseInputcontainer.appendChild(userInputExpenses);
-
-    let row = {};
-    row.index = userInputExpenses.dataset.index;
-    expenseInput.addEventListener('blur', () => {
-        console.log(expenseInput.value)
-        row.expense = expenseInput.value;
-        console.log(row);
-        saveToDatabase(row);
-    })
-    amountInput.addEventListener('blur', () => {
-        row.amount = amountInput.value;
-        console.log(row);
-        saveToDatabase(row);
-    })
-    checkBoxInput.addEventListener('change', () => {
-        row.isChecked = checkBoxInput.checked
-        console.log(row);
-        saveToDatabase(row);
-    })
-
-    deleteButton.addEventListener('click', (e) => {
-        e.target.parentNode.remove();
-    })
-    userInputExpenses.addEventListener('mouseover', () => {
-        deleteButton.style.visibility = 'visible';
-    })
-
-    userInputExpenses.addEventListener('mouseleave', () => {
-        deleteButton.style.visibility = 'hidden';
-    })
 }
 
 function saveToDatabase(row) {
@@ -169,4 +154,8 @@ function saveToDatabase(row) {
     .then(res => res.json())
     .then(data => console.log('Saved:', data))
     .catch(err => console.error('Save error:', err));
+}
+
+function sortByIndex(expenses) {
+    return expenses.sort((a, b) => a.index - b.index);
 }

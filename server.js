003147bdd8e95ee4491875fb1c1ebac700116544
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-05-12 17:10:34 -0700

    added gitignore

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-// Serve everything in the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json()); // This is crucial. Without it, req.body = undefined sadness.
+app.use(express.json());
 
 
 
 app.post('/index', (req, res) => {
   const deletedIndex = Number(req.body.deletedIndex);
-  console.log('Received index:', deletedIndex);
 
-  fs.readFile('public/data.txt', 'utf8', (err, data) => {
+  fs.readFile('database/demo/data.txt', 'utf8', (err, data) => {
     const currentdb = JSON.parse(data);
     console.log(currentdb)
     const filteredData = currentdb.filter(obj => obj.index !== deletedIndex);
@@ -27,7 +25,7 @@ app.post('/index', (req, res) => {
     }
     console.log(filteredData);
 
-    fs.writeFile('public/data.txt', JSON.stringify(filteredData, null, 2), (err) => {
+    fs.writeFile('database/demo/data.txt', JSON.stringify(filteredData, null, 2), (err) => {
       res.sendStatus(204)
     });
 
@@ -39,7 +37,7 @@ app.post('/update-row', (req, res) => {
   const updatedRow = req.body;
   console.log('Got updated row from client:', updatedRow);
 
-  fs.readFile('public/data.txt', 'utf8', (err, data) => {
+  fs.readFile('database/demo/data.txt', 'utf8', (err, data) => {
     const currentdb = JSON.parse(data);
     console.log(currentdb)
     console.log(updatedRow.index)
@@ -48,7 +46,7 @@ app.post('/update-row', (req, res) => {
     currentdb[updatedIndex] = updatedRow;
     console.log(currentdb)
 
-    fs.writeFile('public/data.txt', JSON.stringify(currentdb, null, 2), (err) => {
+    fs.writeFile('database/demo/data.txt', JSON.stringify(currentdb, null, 2), (err) => {
   
     });
     
@@ -63,13 +61,13 @@ app.post('/new-row', (req, res) => {
   const newRow = req.body;
   console.log('Got updated row from client:', newRow);
 
-  fs.readFile('public/data.txt', 'utf8', (err, data) => {
+  fs.readFile('database/demo/data.txt', 'utf8', (err, data) => {
     const currentdb = JSON.parse(data);
     console.log(currentdb)
     currentdb.push(newRow)
     console.log(currentdb)
 
-    fs.writeFile('public/data.txt', JSON.stringify(currentdb, null, 2), (err) => {
+    fs.writeFile('database/demo/data.txt', JSON.stringify(currentdb, null, 2), (err) => {
   
     });
 

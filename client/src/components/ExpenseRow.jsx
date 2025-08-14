d1b8319b978879240ca18ab1fe4019a928a12d4c
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-13 22:55:06 -0700

    expense table complete

--- client/src/components/ExpenseRow.jsx
+++ client/src/components/ExpenseRow.jsx
@@ -52,6 +52,379 @@ const ExpenseRow = () => {
                 </div>
             </div>
 
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
             <div className="expense-row-container">
                 <div className="expense-row-inputs">
                     <input type="text" name="expense-name"></input>

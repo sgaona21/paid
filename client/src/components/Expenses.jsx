10dd4e340506f1ee31ae85cdeec3541dbdbc56d5
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-14 11:19:40 -0700

    total/remaining expense boxes created

--- client/src/components/Expenses.jsx
+++ client/src/components/Expenses.jsx
@@ -3,6 +3,7 @@ import ExpenseRow from "./ExpenseRow";
 const Expenses = () => {
   return (
     <div className="expense-content-container">
+
       <div className="expense-table-container">
         <ul className="expense-table-headers">
           <li>Expense</li>
@@ -11,6 +12,20 @@ const Expenses = () => {
         </ul>
         <ExpenseRow />
       </div>
+
+     <div className="monthly-total-container">
+        <div className="monthly-total-label">Total Expenses</div>
+        <div className="monthly-total-amount">0</div>
+      </div>
+
+      <div className="monthly-remaining-container">
+        <div className="monthly-remaining-label">Remaining Expenses</div>
+        <div className="monthly-remaining-amount">0</div>
+      </div>
+
+     
+
+
     </div>
   );
 };

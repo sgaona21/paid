748dd607b313da0a1f1bc946c138db3bbc6c7c92
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-13 21:55:31 -0700

    expense table css applied

--- client/src/components/ExpenseRow.jsx
+++ client/src/components/ExpenseRow.jsx
@@ -39,6 +39,30 @@ const ExpenseRow = () => {
                     <input type="checkbox" name="expense-paid"></input>
                 </div>
             </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
+
+            <div className="expense-row-container">
+                <div className="expense-row-inputs">
+                    <input type="text" name="expense-name"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="number" name="expense-amount"></input>
+                </div>
+                <div className="expense-row-inputs">
+                    <input type="checkbox" name="expense-paid"></input>
+                </div>
+            </div>
         </div>
     )
 }

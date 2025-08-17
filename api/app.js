f9275a6b402dbd66b3a8b5df4d508e8964b33520
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-16 19:50:32 -0700

    api/cors/ setup - startup files and main updated in package.json

--- api/app.js
+++ api/app.js
@@ -1,4 +1,57 @@
 const express = require('express');
 const morgan = require("morgan");
+const { sequelize } = require('./models');
+const cors = require('cors');
 
+
+
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 app.use(morgan("dev"));
+
+//test db connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from the API',
+  });
+});
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
+
+// setup a global error handler
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {},
+  });
+});
+
+// set our port
+app.set('port', process.env.PORT || 5002);
+
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+});

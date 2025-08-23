4fabcee3f2297a84786c2f256fcda9e72647f84c
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-23 12:04:27 -0700

    header title centered with absolute positioning. welcome message and logout button added to header for logout functionality

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -7,9 +7,17 @@ import Expenses from './Expenses';
 const Layout = () => {
     return (
         <main className='layout-container'>
-            <section className='dashboard'></section>
+            <section className='dashboard'>
+                <h2>Paid.com</h2>
+            </section>
 
-            <section className='header'></section>
+            <section className='header'>
+                <h2>Monthly Expenses</h2>
+                <div className='logged-in'>
+                    <div className='welcome'>Welcome, Steven</div>
+                    <div className='logout'>Log Out</div>
+                </div>
+            </section>
 
             <section className='content'>
                 <Outlet />

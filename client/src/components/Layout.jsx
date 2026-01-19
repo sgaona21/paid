df73bcb9f6fe73d83d35691dac39a8c2a32aee69
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-01-19 15:23:14 -0700

    added conditional rendering for expense page based on currentuser

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,19 @@
 import '../styles/layout.css';
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 import Expenses from './Expenses';
 
 
 const Layout = () => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    if (currentUser === null) {
+        return <Navigate to="/login" replace />
+    }
+
     return (
+        
         <main className='layout-container'>
             <section className='dashboard'>
                 <h2>Paid</h2>
@@ -14,7 +22,7 @@ const Layout = () => {
             <section className='header'>
                 <h2>Monthly Expenses</h2>
                 <div className='logged-in'>
-                    <div className='welcome'>Welcome, Steven</div>
+                    <div className='welcome'>Welcome, {currentUser}</div>
                     <div className='logout'>Log Out</div>
                 </div>
             </section>

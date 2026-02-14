04f0bca69f529595ba9e3ecfcc187caae1d290c2
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-02-14 14:14:08 -0700

    deleted worthless container div - refactored expenseRow

--- client/src/components/ExpenseRow.jsx
+++ client/src/components/ExpenseRow.jsx
@@ -1,43 +1,43 @@
-
-
 const ExpenseRow = ({ row, index, handleRowChange, deleteRow, saveRow }) => {
-    return (
-        // <div className="expense-rows-container">
-            <div className="expense-row-container">
-                <div className="expense-row-inputs">
-                    <input 
-                    type="text" 
-                    name="expense-name"
-                    value={row?.name ?? ""}
-                    onChange={(e) => handleRowChange(index, "name", e.target.value)}
-                    onBlur={() => saveRow(row)}
-                     ></input>
-                </div>
-                <div className="expense-row-inputs">
-                    <input 
-                    type="number" 
-                    name="expense-amount"
-                    value={row?.amount ?? ""}
-                    onChange={(e) => handleRowChange(index, "amount", Number(e.target.value))}
-                    onBlur={() => saveRow(row)}
-                    ></input>
-                </div>
-                <div className="expense-row-inputs">
-                    <input 
-                    type="checkbox" 
-                    name="expense-paid"
-                    checked={!!row?.isPaid}
-                    onChange={(e) =>  {
-                        handleRowChange(index, "isPaid", e.target.checked);
-                        saveRow({ ...row, isPaid: e.target.checked });
-                    }}
-                    onBlur={() => saveRow(row)}
-                    ></input>
-                    <div className="delete-row" onClick={deleteRow} >❌</div>
-                </div>
-            </div>
-        // </div>
-    )
-}
+  return (
+    <div className="expense-row-container">
+      <div className="expense-row-inputs">
+        <input
+          type="text"
+          name="expense-name"
+          value={row?.name ?? ""}
+          onChange={(e) => handleRowChange(index, "name", e.target.value)}
+          onBlur={() => saveRow(row)}
+        ></input>
+      </div>
+      <div className="expense-row-inputs">
+        <input
+          type="number"
+          name="expense-amount"
+          value={row?.amount ?? ""}
+          onChange={(e) =>
+            handleRowChange(index, "amount", Number(e.target.value))
+          }
+          onBlur={() => saveRow(row)}
+        ></input>
+      </div>
+      <div className="expense-row-inputs">
+        <input
+          type="checkbox"
+          name="expense-paid"
+          checked={!!row?.isPaid}
+          onChange={(e) => {
+            handleRowChange(index, "isPaid", e.target.checked);
+            saveRow({ ...row, isPaid: e.target.checked });
+          }}
+          onBlur={() => saveRow(row)}
+        ></input>
+        <div className="delete-row" onClick={deleteRow}>
+          ❌
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseRow;

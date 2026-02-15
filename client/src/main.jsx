2b38c96a77355ee652b03af74a055a3dacde10fe
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-02-15 11:47:16 -0700

    installed chart.js - removed margin from dash options

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { UserProvider } from './auth/UserContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { UserProvider } from './auth/UserContext.jsx';
+import './components/chartSetup.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

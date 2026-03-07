6123cace56c0e5199d6cc3ca880009f399f7fd25
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-03-07 16:00:18 -0700

    added deployment domain to CORS

--- api/app.js
+++ api/app.js
@@ -8,10 +8,19 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 
 const app = express();
 
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://paidbudget.com"
+  ],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(morgan("dev"));
 

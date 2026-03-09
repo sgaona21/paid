6bce8686762454538dfb75bda38fda1c6be017e3
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-03-08 19:13:08 -0700

    created user table and added migration

---
+++ api/models/sheet.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Sheet = sequelize.define(
+    "Sheet",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      label: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        defaultValue: "New Sheet",
+      },
+      netIncome: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Sheets",
+      timestamps: true,
+    }
+  );
+
+  Sheet.associate = function (models) {
+    Sheet.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+
+    // later:
+    // Sheet.hasMany(models.Expense, { foreignKey: "sheetId" })
+  };
+
+  return Sheet;
+};

ec92fa32139abc7c07dc0630364c251a36b7e0a2
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2026-03-10 13:07:02 -0700

    added error code to authjwt

--- api/middleware/auth-jwt.js
+++ api/middleware/auth-jwt.js
@@ -16,11 +16,12 @@ const { User } = require("../models");
 // };
 
 exports.authJwt = async (req, res, next) => {
+  console.log("hey dude i ran lol")
   try {
     const token = req.cookies?.auth;
-    
+    console.log("HERE IS THE COOKIE:", token);
     if (!token) {
-      return res.status(401).json({ message: "Authentication required" });
+      return res.status(401).json({ message: "From authJwt: Cookie Invalid or Missing" });
     }
 
     const payload = jwt.verify(token, process.env.JWT_SECRET);
